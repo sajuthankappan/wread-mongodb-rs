0ecfb99d3daf13b0777e685027ef1ab7e84c46cc
Saju Thankappan <sajuthankappan@gmail.com>
2022-03-18 18:41:41 +0530

    Add CrudRepository trait

--- src/crud_repository.rs
+++ src/crud_repository.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use futures::stream::StreamExt;
 use log::trace;
 use mongodb::bson::oid::ObjectId;
@@ -16,6 +17,233 @@ use mongodb::{
     results::{DeleteResult, InsertOneResult, UpdateResult},
 };
 use std::borrow::Borrow;
+
+#[async_trait]
+pub trait CrudRepository {
+    fn collection_name(&self) -> String;
+    fn database(&self) -> Box<&Database>;
+
+    async fn find_one<T>(&self, filter_document: Document) -> Result<Option<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        let coll = self.database().collection(&self.collection_name());
+        coll.find_one(filter_document, None).await
+    }
+
+    async fn find_by_id<T>(&self, id: &ObjectId) -> Result<Option<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        let filter_document = doc! {"_id":  id};
+        self.find_one(filter_document).await
+    }
+
+    async fn find_by_string_id<T>(&self, id: &str) -> Result<Option<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        let filter_document = doc! {"_id": id};
+        self.find_one(filter_document).await
+    }
+
+    async fn find_one_by_string_field<T>(&self, name: &str, value: &str) -> Result<Option<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        let filter_document = doc! {name: value};
+        self.find_one(filter_document).await
+    }
+
+    async fn find_by_string_field<T>(&self, name: &str, value: &str) -> Result<Vec<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        let filter_document = doc! {name: value};
+        self.find_simple(filter_document).await
+    }
+
+    async fn find_all<T>(&self) -> Result<Vec<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        self.find(None, None).await
+    }
+
+    async fn find_simple<T>(&self, filter_document: Document) -> Result<Vec<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        trace!("find");
+        self.find(filter_document, None).await
+    }
+
+    async fn find_with_sort<T>(
+        &self,
+        filter_document: Document,
+        sort_document: Document,
+    ) -> Result<Vec<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        let sort_option = get_sort_find_option(Some(sort_document));
+        self.find(filter_document, sort_option).await
+    }
+
+    async fn find<T>(
+        &self,
+        filter_document: impl Into<Option<Document>> + Send + 'async_trait,
+        options: impl Into<Option<FindOptions>> + Send + 'async_trait,
+    ) -> Result<Vec<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        let coll = self.database().collection(&self.collection_name());
+
+        let mut cursor = coll.find(filter_document, options).await?;
+        let mut items = Vec::<T>::new();
+        while let Some(result) = cursor.next().await {
+            match result {
+                Ok(document) => {
+                    items.push(document);
+                }
+                Err(err) => {
+                    return Err(err);
+                }
+            }
+        }
+
+        Ok(items)
+    }
+
+    async fn count_documents(
+        &self,
+        filter: impl Into<Option<Document>> + Send + 'async_trait,
+    ) -> Result<u64, Error> {
+        let coll = &self
+            .database()
+            .collection::<Document>(&self.collection_name());
+        coll.count_documents(filter, None).await
+    }
+
+    async fn aggregate<T>(
+        &self,
+        pipeline: impl IntoIterator<Item = Document> + Send + 'async_trait,
+        options: impl Into<Option<AggregateOptions>> + Send + 'async_trait,
+    ) -> Result<Vec<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        let coll = self
+            .database()
+            .collection::<Document>(&self.collection_name());
+        let mut cursor = coll.aggregate(pipeline, options).await?;
+        let mut items = Vec::<T>::new();
+
+        while let Some(result) = cursor.next().await {
+            match result {
+                Ok(document) => {
+                    let item = bson::from_bson::<T>(BsonDocument(document))?;
+                    items.push(item);
+                }
+                Err(err) => {
+                    return Err(err);
+                }
+            }
+        }
+
+        Ok(items)
+    }
+
+    fn get_sort_find_option(&self, sort_document_option: Option<Document>) -> Option<FindOptions> {
+        if let Some(sort_document) = sort_document_option {
+            Some(FindOptions::builder().sort(sort_document).build())
+        } else {
+            None
+        }
+    }
+
+    async fn _find_one_by_field<T>(
+        &self,
+        field_name: String,
+        value: String,
+    ) -> Result<Option<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Unpin + Send + Sync,
+    {
+        self.find_one(doc! {field_name: value}).await
+    }
+
+    async fn add<T>(
+        &self,
+        item: impl Borrow<T> + Send + Sync + 'async_trait,
+    ) -> Result<InsertOneResult, Error>
+    where
+        T: Serialize + Send + Sync,
+    {
+        let coll = self.database().collection(&self.collection_name());
+        coll.insert_one(item, None).await
+    }
+
+    async fn update_one(
+        &self,
+        query: Document,
+        update: impl Into<UpdateModifications> + Send + Sync + 'async_trait,
+        options: impl Into<Option<UpdateOptions>> + Send + Sync + 'async_trait,
+    ) -> Result<UpdateResult, Error> {
+        let coll = self.database().collection::<Document>(&self.collection_name());
+        coll.update_one(query, update, options).await
+    }
+
+    async fn find_one_and_update<T>(
+        &self,
+        filter: Document,
+        update: impl Into<UpdateModifications> + Send + Sync + 'async_trait,
+        options: impl Into<Option<FindOneAndUpdateOptions>> + Send + Sync + 'async_trait,
+    ) -> Result<Option<T>, Error>
+    where
+        for<'a> T: DeserializeOwned + Send + Sync,
+    {
+        let coll = self.database().collection(&self.collection_name());
+        coll.find_one_and_update(filter, update, options).await
+    }
+
+    async fn find_one_and_replace<T>(
+        &self,
+        filter: Document,
+        replacement: impl Borrow<T> + Send + Sync + 'async_trait,
+        options: impl Into<Option<FindOneAndReplaceOptions>> + Send + Sync + 'async_trait,
+    ) -> Result<Option<T>, Error>
+    where
+        T: Serialize + DeserializeOwned + Send + Sync,
+    {
+        let coll = self.database().collection(&self.collection_name());
+        coll.find_one_and_replace(filter, replacement, options)
+            .await
+    }
+
+    async fn replace_one<T>(
+        &self,
+        query: Document,
+        replacement: impl Borrow<T> + Send + Sync + 'async_trait,
+        options: impl Into<Option<ReplaceOptions>> + Send + Sync + 'async_trait,
+    ) -> Result<UpdateResult, Error>
+    where
+        for<'a> T: Serialize + Send + Sync,
+    {
+        let coll = self.database().collection(&self.collection_name());
+        coll.replace_one(query, replacement, options).await
+    }
+
+    async fn delete_one(
+        &self,
+        query: Document,
+        options: impl Into<Option<DeleteOptions>> + Send + Sync + 'async_trait,
+    ) -> Result<DeleteResult, Error> {
+        let coll = self.database().collection::<Document>(&self.collection_name());
+        coll.delete_one(query, options).await
+    }
+}
 
 pub async fn find_one<T>(
     filter_document: Document,
